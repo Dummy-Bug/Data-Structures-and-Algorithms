06a44e8cfe44d3680916a55650d5f445a767c853
Laxit Rana <laxyrajput@gmail.com>
2021-09-14 16:25:18 +0530

    Initial Commit

---
+++ Dynamic Programming/Longest Common Subsequence/1.2 LCS BUP.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        
+        dp = [[-1 for j in range(len(text2)+1)] for i in range(len(text1)+1) ]
+        
+        for i in range(len(text1)+1): # if any string is empty len(lcs) == 0
+            dp[i][0] = 0 
+            
+        for j in range(len(text2)+1): # if any string is empty len(lcs) == 0
+            dp[0][j] = 0
+            
+        
+        for i in range(1,len(text1)+1):
+            for j in range(1,len(text2)+1):
+                
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j]  =  1 + dp[i-1][j-1]
+                    
+                else:
+                    dp[i][j]  = max(dp[i-1][j],dp[i][j-1])
+        
+        return dp[-1][-1]
+        

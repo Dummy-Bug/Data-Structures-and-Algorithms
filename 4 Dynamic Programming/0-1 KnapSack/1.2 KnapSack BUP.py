98d4b984841e3fa89466eaae6ccc525b91164783
Laxit Rana <laxyrajput@gmail.com>
2022-03-31 14:56:25 +0530

    Initial Commit

--- 4 Dynamic Programming/0-1 KnapSack/1.2 KnapSack BUP.py
+++ 4 Dynamic Programming/0-1 KnapSack/1.2 KnapSack BUP.py
@@ -1,18 +1,20 @@
+https://practice.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1#
+
 class Solution:
+    
     def knapSack(self,W, wt, val, n):
         
-        dp = [[0 for i in range(W+1)] for j in range(n+1)]
-      
+        dp = [[0 for j in range(W+1)] for i in range(n+1)]
+        
         for i in range(1,n+1):
             for j in range(1,W+1):
                 
-                if wt[i-1] <= j:
+                if j >= wt[i-1]:
                     
-                    taken = val[i-1]+dp[i-1][j-wt[i-1]]
-                    not_taken = dp[i-1][j]
-                    dp[i][j] = max(taken,not_taken)
+                    dp[i][j] = max(val[i-1] + dp[i-1][j-wt[i-1]],dp[i-1][j])
                     
+                    # just put dp array in place of recursive call 
+                
                 else:
                     dp[i][j] = dp[i-1][j]
                     
-        return dp[-1][-1]

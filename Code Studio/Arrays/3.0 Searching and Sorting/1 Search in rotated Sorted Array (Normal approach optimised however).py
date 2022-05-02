1545d749843b39e9d1e2e7fedcac1c7f4dd784b4
Laxit Rana <laxyrajput@gmail.com>
2022-05-02 09:26:05 +0530

    Updated Files

--- Code Studio/Arrays/3.0 Searching and Sorting/1 Search in rotated Sorted Array (Normal approach optimised however).py
+++ Code Studio/Arrays/3.0 Searching and Sorting/1 Search in rotated Sorted Array (Normal approach optimised however).py
@@ -30,13 +30,14 @@ def getPivot(nums):
     hi  = len(nums)-1
 	
 	# Try to solve binary search when low <= hi , you will get more clarity.
-	# we can also think of this function as finding where the smalles value lies in the array.
+	# we can also think of this function as finding where the smallest value lies in the array, as it will 
+    # tell us the begining of the array and then we can use modulus operator to get the position of the element.
     while low <= hi: # we have to use make it <= because when the array size if 1 then it will return low
 		
         mid = (low+hi)//2
             
-        if nums[mid] >= nums[low] and nums[mid] <= nums[hi]:
-            return low
+        if nums[mid] >= nums[low] and nums[mid] <= nums[hi]: 
+            return low # return low as we want the index of smallest value in the array.
         else: 
             if  nums[mid] > nums[hi]: 
                 low = mid + 1

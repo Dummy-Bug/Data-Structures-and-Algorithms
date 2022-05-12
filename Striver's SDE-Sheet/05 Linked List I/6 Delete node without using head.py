428863fd322a9121774c121641c258b0a60e57cb
Laxit Rana <laxyrajput@gmail.com>
2022-05-12 23:40:41 +0530

    Initial Commit

---
+++ Striver's SDE-Sheet/05 Linked List I/6 Delete node without using head.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/delete-node-in-a-linked-list/
+class Solution:
+    def deleteNode(self, node):
+        
+        node.val  =  node.next.val
+        node.next =  node.next.next
+        

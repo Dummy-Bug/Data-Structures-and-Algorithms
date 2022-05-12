6ec63273e99bf12866608b37c08bfe61167956b8
Laxit Rana <laxyrajput@gmail.com>
2022-05-13 02:00:19 +0530

    Initial Commit

---
+++ Striver's SDE-Sheet/05 Linked List I/5 Add two number in Linked Lists.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/add-two-numbers/
+# check LC notes for other solution as well.
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        carry = 0
+        root = n = ListNode(0)
+        while l1 or l2 or carry:
+            v1 = v2 = 0
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
+            carry, val = divmod(v1+v2+carry, 10)
+            n.next = ListNode(val)
+            n = n.next
+        return root.next

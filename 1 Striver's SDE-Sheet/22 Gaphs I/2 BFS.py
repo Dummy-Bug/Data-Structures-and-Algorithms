158acd6267aff944de906edd4d9267baa25bcddd
Laxit Rana <laxyrajput@gmail.com>
2022-05-25 11:22:19 +0530

    Initial Commit

--- 1 Striver's SDE-Sheet/22 Gaphs I/2 BFS.py
+++ 1 Striver's SDE-Sheet/22 Gaphs I/2 BFS.py
@@ -1,27 +1,30 @@
-# https://practice.geeksforgeeks.org/problems/bfs-traversal-of-graph/1
+# https://practice.geeksforgeeks.org/problems/bfs-traversal-of-graph/1#
+
+from queue import Queue
 class Solution:
     def bfsOfGraph(self, V, adj):
         
         
-        visited = dict()
-        
+        visited = [False for i in range(V)]
         result = []
+        
         self.bfs(V,adj,visited,result)
         return result
     
     def bfs(self,V,neighbors,visited,result):
-        from collections import deque
-        q = deque([0])
+        
+        q = Queue(maxsize=0) # initializing infinte sized queue
+        q.put(0)
         visited[0] = True
         
-        while q:
-            vertex = q.popleft()
+        while (q.empty() == False):
+            vertex = q.get()
             
             for neighbor in neighbors[vertex]:
                 
-                if neighbor not in visited:
+                if visited[neighbor] == False:
                     visited[neighbor] = True
-                    q.append(neighbor)
+                    q.put(neighbor)
                     
             result.append(vertex)
-        return result
+        return 

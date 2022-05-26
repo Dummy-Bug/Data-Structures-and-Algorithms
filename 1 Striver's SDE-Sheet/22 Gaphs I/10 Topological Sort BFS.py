3f0680302c78adf4cb282aaf2ae8da298295caa8
Laxit Rana <laxyrajput@gmail.com>
2022-05-26 14:25:46 +0530

    Initial Commit

---
+++ 1 Striver's SDE-Sheet/22 Gaphs I/10 Topological Sort BFS.py
@@ -0,0 +1,35 @@
+# https://practice.geeksforgeeks.org/problems/topological-sort/1
+class Solution:
+    def topoSort(self, V, adj):
+        
+        self.indegree = [0]*V
+        
+        for i in range(V):
+            for padosi in adj[i]:
+                self.indegree[padosi] += 1
+                
+        self.queue = deque() 
+        for i  in range(V):
+            
+            if self.indegree[i] == 0:
+                self.queue.append(i) 
+                
+        self.visited = [False for i in range(V)]
+        
+        return self.bfs(adj,[])
+        
+    def bfs(self,adj,result):
+        
+        while self.queue:
+            
+            u = self.queue.popleft()
+            self.visited[u] = True
+            result.append(u)
+            for padosi in adj[u]:
+                
+                if self.visited[padosi] == False:
+                    self.indegree[padosi] -= 1
+                    
+                    if self.indegree[padosi] == 0:
+                        self.queue.append(padosi)
+        return result

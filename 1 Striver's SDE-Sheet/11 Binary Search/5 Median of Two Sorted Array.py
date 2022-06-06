4ae097a4f2e43c7a5002a40f6fc3dcc7546dda0e
Laxit Rana <laxyrajput@gmail.com>
2022-06-06 23:02:19 +0530

    Initial Commit

--- 1 Striver's SDE-Sheet/11 Binary Search/5 Median of Two Sorted Array.py
+++ 1 Striver's SDE-Sheet/11 Binary Search/5 Median of Two Sorted Array.py
@@ -1,34 +1,37 @@
 # https://leetcode.com/problems/median-of-two-sorted-arrays/
 class Solution:
-    def findMedianSortedArrays(self,arr1, arr2):
-        n1 = len(arr1)
-        n2 = len(arr2)
-
-        if n1 > n2:
-            arr1, arr2 = arr2, arr1
-            n1, n2 = n2, n1
-
-        low = 0
-        high = n1
-
-        while low <= high:
-            cut1 = low + (high - low) // 2 
-            cut2 = (n1 + n2 + 1) // 2 - cut1 
-
-            l1 = -float('inf') if cut1 == 0 else arr1[cut1-1]
-            l2 = -float('inf') if cut2 == 0 else arr2[cut2-1]
-
-            r1 = float('inf') if cut1 == n1 else arr1[cut1]
-            r2 = float('inf') if cut2 == n2 else arr2[cut2]
-
-            if l1 <= r2 and l2 <= r1:
-                if (n1+n2)%2 == 0:
-                    return (max(l1, l2) + min(r1, r2)) / 2
+    def findMedianSortedArrays(self,nums1, nums2):
+      
+        
+        if len(nums1) > len(nums2):
+            return self.findMedianSortedArrays(nums2,nums1)
+            
+        
+        low, high = 0, len(nums1) # Have to take whole length 
+        desired   = (len(nums1)+len(nums2)+1)//2
+        
+        while low <= high :
+            
+            count1 = (low+high)>>1 # give us number of elements to take from first Array
+            
+            count2 = (desired-count1)
+            # print(count1,count2)
+            left1 = nums1[count1-1] if count1 != 0 else float('-inf')
+            left2 = nums2[count2-1] if count2 != 0 else float('-inf')
+            
+            right1 = nums1[count1] if count1 != len(nums1) else float('inf')
+            right2 = nums2[count2] if count2 != len(nums2) else float('inf')
+            
+            # print(left1,right2,left2,right1)
+            if left1 <= right2 and left2 <= right1:
+                
+                if ( len(nums1)+len(nums2) ) %2 == 0: 
+                    return ( max(left1,left2) + min(right1,right2) )/2.0
                 else:
-                    return max(l1, l2)
-            elif l1 > l2:
-                high = cut1 - 1
-            else:
-                low = cut1 + 1
-
-        return -1
+                    return max(left1,left2)
+            
+            elif left1 > right2:
+                high = count1 - 1
+            
+            elif left2 > right1:
+                low = count1 + 1

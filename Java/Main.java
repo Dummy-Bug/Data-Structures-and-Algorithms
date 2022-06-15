086fd803883112909cae69720f0b6893fd2e883d
Laxit Rana <laxyrajput@gmail.com>
2022-06-15 19:35:13 +0530

    Initial Commit

---
+++ Java/Main.java
@@ -0,0 +1,11 @@
+public class Main 
+{
+//  class name should be same as that of file name in java
+
+    public static void main(String [] args)
+    {
+        System.out.println("Hello World");
+        // Put Semicolns after every statements
+
+    }
+}

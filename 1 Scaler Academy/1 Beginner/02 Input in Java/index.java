60c0b29efb31523f4c48674f53dc048972375dc4
Laxit Rana <laxyrajput@gmail.com>
2022-07-19 11:22:44 +0530

    Initial Commit

---
+++ 1 Scaler Academy/1 Beginner/02 Input in Java/index.java
@@ -0,0 +1,142 @@
+import java.util.*;
+
+class Main {
+	public static void main(String args[]) { 
+		Scanner sc = new Scanner(System.in);
+		float a = (10f/3);
+		/*
+		To scan inputs:
+		1. create a scanner(variable).
+		2. use the appropriate functionality to scan your data
+		*/
+		/*
+		
+		//int n = sc.nextInt();
+		long n = sc.nextLong();
+		
+		System.out.println("value given by user is :"+n);
+		*/
+		// Your code goes here
+		/*
+		int month = 6;
+		System.out.println("month = "+month); //6
+		//System.out.println(month);
+		month = 10;
+		System.out.println("month = " + month); //10
+		
+		month = 9;
+		System.out.println("month = "+month);//9
+		
+		month = 6;
+		System.out.println("month = "+month);//6
+		*/
+		//int month = 10;
+		
+		
+		/*
+		int black_grapes_; //valid
+		int sun$shine; //valid
+  		int 7a; //invalid
+		int _roll_number$ ;//valid
+  		int 4twenty ;//invalid
+  		int #age; //invalid
+  		int spring roll; //invalid
+  		int _delhi ;//valid
+  		int roll_number; //valid
+  		int bus ticket; //invalid
+  		int static ;//invalid
+  		int void; //invalid
+  		int $$$$; //valid
+		  */
+		
+		
+		
+		
+		
+		//int Month = 8;
+		//int mOnth = 10;
+		//int MONTH = 10;
+		/*
+		1. Only _ and $ special characters can be in the name of a variable. 
+		2. Numbers can be there but not in the start
+		3. Variable names are case sensitive
+		*/
+		//System.out.println("The current month is "+month);
+		
+		
+		/*
+		Print sum of two variables?
+		
+		*/
+		/*
+		int a = 10;
+		int b = 3;
+		
+		System.out.println("Sum of a and b is :"+(a+b));
+		
+		int x = 10, y = 9;
+		System.out.println("Product of x and y is:"+(x*y));
+		
+		long population = 1000000000000000000l;
+		System.out.println("Population of India is:"+population);
+		/*
+		Max value of int +2,147,483,647
+		Min value of int -2,147,483,648
+		
+		Max value of long +9,223,372,036,854,775,807
+		Min value of long -9,223,372,036,854,775,808 
+		
+		float var1=10, var2=0;
+		System.out.println("Ans of var1/var2 is: "+var1/var2);
+		//float can store only 7 digits to decimal
+		
+		double a1=10, a2=0;
+		System.out.println("Ans of a1/a2 is: "+a1/a2);
+		//double can store till 16 digits after decimal
+		/*
+		For decimal numbers / floating type numbers
+		
+		float
+		*/
+		
+		/*
+		Write a program to take two inputs l and b and return the area of the rectangle
+		*/
+		
+		int l = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println("Area of the rectangle is : "+(l*b));
+		
+		/*
+		Calculate area of a circle?
+		input? radius?
+		*/
+				
+	}
+	
+	
+	
+	
+	
+	
+	//what if ...
+	
+	//Dr.Kalam 
+	/*
+	Never ask anyone till you yourself fail to find the ans
+	-> Try out(Write code and observe the output)
+	-> Find the ans of why? 
+			Google (Stackoverflow)
+			Ask(Peers, Instructor, TAs)
+	*/
+	
+	// After these slashes
+	/* Inside these */
+	
+	//Comments^
+	
+	/* Pune Baner -> Thane 
+	
+	*/
+	
+}

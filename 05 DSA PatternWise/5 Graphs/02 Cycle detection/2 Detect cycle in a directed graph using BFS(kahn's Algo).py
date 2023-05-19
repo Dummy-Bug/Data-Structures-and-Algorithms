9ad7fd549259654fea8ee59952cbcf33e3251145
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2023-05-19 14:49:52 +0530

    Update 2 Detect cycle in a directed graph using BFS(kahn's Algo).py

--- 05 DSA PatternWise/5 Graphs/02 Cycle detection/2 Detect cycle in a directed graph using BFS(kahn's Algo).py
+++ 05 DSA PatternWise/5 Graphs/02 Cycle detection/2 Detect cycle in a directed graph using BFS(kahn's Algo).py
@@ -1,4 +1,3 @@
-#  This Solution is Wrong and moreover kahn's algorithm is for topological sorting which is for directed graphs not for undirected graphs.
 
 class Solution:
     def isCyclic(self, V, adj):
